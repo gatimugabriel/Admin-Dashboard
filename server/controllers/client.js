c1b332b87397a13d4f8d541c08e855d1cc524a5b
Gabriel <geraldg652@gmail.com>
2023-08-20 09:58:09 +0300

    created cutomers page (server + client)

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,4 +1,5 @@
 import Product from "../models/Product.js";
+import User from "../models/User.js";
 import ProductStat from "../models/ProductStat.js";
 
 export const getProducts = async (req, res) => {
@@ -19,6 +20,15 @@ export const getProducts = async (req, res) => {
     );
 
     res.status(200).json(producsWithStats);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
+
+export const getCustomers = async (req, res) => {
+  try {
+    const customers = await User.find({ role: "user" }).select("-password")
+    res.status(200).json(customers);
   } catch (err) {
     res.status(404).json({ message: err.message });
   }
